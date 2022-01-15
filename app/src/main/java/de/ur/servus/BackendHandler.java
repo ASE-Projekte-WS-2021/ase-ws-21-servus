81fd40b3e17d857a04bf64b84411f576ed8b0cd4
Shoggomo <andy-hot@web.de>
2022-01-15 17:05:58 +0100

    #12 Created Event Datatype and Firebase backend to subscribe to event data; Added example usage of Firebase backend

--- app/src/main/java/de/ur/servus/BackendHandler.java
+++
@@ -1,6 +0,0 @@
-package de.ur.servus;
-
-public interface BackendHandler {
-    // load and filter events (needs Event and Filter class)
-    // add events
-}
--- app/src/main/java/de/ur/servus/FirestoreBackendHandler.java
+++
@@ -1,5 +0,0 @@
-package de.ur.servus;
-
-public class FirestoreBackendHandler {
-
-}
