5723cd0d03d456916a88f01b481c0cf5e40e2402
Arne Tiedemann <tie.arne@gmail.com>
2022-02-06 17:27:06 +0100

    Issue #68: - Tutorial Screen created - Tutorial opens on first ever startup of the app - Location Management -> Runtime Fallback, if location service is not found (e.g. user disables it manually while app is running) - NOT IMPLEMENTED YET: Account Creation Dialog
    Issue #70:
    - Handled permission denial and location fallbacks
    
    Issue #71:
    - Visible TODO sections hidden
    
    ONGOING BUGFIXES:
    - Fixed Map "jumps" when device's Soft Keyboard expands
    - Fixed/Handled Network Status errors (Issue #72)

--- app/src/main/java/de/ur/servus/PermissionDeniedActivity.java
+++ app/src/main/java/de/ur/servus/PermissionDeniedActivity.java
@@ -2,13 +2,27 @@ package de.ur.servus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.widget.Button;
 
 public class PermissionDeniedActivity extends AppCompatActivity {
+
+    Button linkToPermissionSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_permission_denied);
+
+        linkToPermissionSettings = findViewById(R.id.btn_permission_settings);
+        linkToPermissionSettings.setOnClickListener(v -> {
+            Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                    Uri.fromParts("package", getPackageName(), null));
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        });
     }
 }
